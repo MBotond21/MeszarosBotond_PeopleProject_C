d81437275bdd7c2d71368ce5adba0a73554f21a3
Mészáros Botond <m.botond07@gmail.com>
2024-05-27 12:50:06 +0200

    Helytelen értékek gátlása a Person classban

--- PeopleProject/Person.cs
+++ PeopleProject/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,17 +17,17 @@ namespace PeopleProject
 
         public Person(int id, string name, int age, bool isStudent, int score)
         {
-            this.id = id;
-            this.name = name;
-            this.age = age;
-            this.isStudent = isStudent;
-            this.score = score;
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.IsStudent = isStudent;
+            this.Score = score;
         }
 
         public int Id { get => id; set => id = value; }
-        public string Name { get => name; set => name = value; }
-        public int Age { get => age; set => age = value; }
+        public string Name { get => name; set { if (value == "") { throw new ArgumentException("A név nem lehet üres!"); } else { name = value; } } }
+        public int Age { get => age; set { if (value < 1) { throw new ArgumentException("Az életkor nem lehet 1-nél kisebb!"); } else { age = value; } } }
         public bool IsStudent { get => isStudent; set => isStudent = value; }
-        public int Score { get => score; set => score = value; }
+        public int Score { get => score; set { if (value < 0) { throw new ArgumentException("A pontszám nem lehet 0-nál kisebb!"); } else { score = value; } } }
     }
 }
